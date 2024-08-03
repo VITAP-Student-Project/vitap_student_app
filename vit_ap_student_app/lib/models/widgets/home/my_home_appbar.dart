cf83fa7343cde2fccb6e93a553773fed0ee6eba6
Udhay Adithya <udhayxd@gmail.com>
2024-08-04 01:34:25 +0530

    changed to slivers

--- vit_ap_student_app/lib/models/widgets/home/my_home_appbar.dart
+++ vit_ap_student_app/lib/models/widgets/home/my_home_appbar.dart
@@ -4,17 +4,14 @@ import 'package:page_transition/page_transition.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../pages/profile/account_page.dart';
 
-class MyHomeAppBar extends StatefulWidget implements PreferredSizeWidget {
-  const MyHomeAppBar({super.key});
+class MyHomeSliverAppBar extends StatefulWidget {
+  const MyHomeSliverAppBar({super.key});
 
   @override
-  _MyHomeAppBarState createState() => _MyHomeAppBarState();
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  _MyHomeSliverAppBarState createState() => _MyHomeSliverAppBarState();
 }
 
-class _MyHomeAppBarState extends State<MyHomeAppBar> {
+class _MyHomeSliverAppBarState extends State<MyHomeSliverAppBar> {
   String? _profileImagePath;
   String? _username;
 
@@ -35,51 +32,66 @@ class _MyHomeAppBarState extends State<MyHomeAppBar> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-                      context,
-                      PageTransition(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeInOut,
-                        type: PageTransitionType.fade,
-                        child: const AccountPage(),
-                      ),
-                    );
-      },
-      child: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        automaticallyImplyLeading: false,
-        title: Row(
-          children: [
-            CircleAvatar(
-              radius: 20,
-              backgroundImage: AssetImage(
-                  _profileImagePath ?? 'assets/images/pfp/default.png'),
-            ),
-            SizedBox(width: 10), // Add space between the avatar and text
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Hello 👋,',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w300,
-                      color: Theme.of(context).colorScheme.primary),
+    return SliverAppBar(
+      automaticallyImplyLeading: false,
+      expandedHeight: 75,
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      flexibleSpace: FlexibleSpaceBar(
+        expandedTitleScale: 1.2,
+        titlePadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        title: Align(
+          alignment: Alignment.bottomLeft,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                PageTransition(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                  type: PageTransitionType.fade,
+                  child: const AccountPage(),
                 ),
-                Text(
-                  '$_username',
-                  style: TextStyle(
-                    fontSize: 20, // Adjust the font size as needed
-                    fontWeight: FontWeight.w600,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
+              );
+            },
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage(
+                      _profileImagePath ?? 'assets/images/pfp/default.png'),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hello 👋,",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    Text(
+                      _username ?? '',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    // Optionally, you can add other details here, such as the number of classes.
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- vit_ap_student_app/lib/pages/features/home_page.dart
+++ vit_ap_student_app/lib/pages/features/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:vit_ap_student_app/models/widgets/home/my_home_appbar.dart';
 import 'package:vit_ap_student_app/models/widgets/home/my_upcoming_class_widget.dart';
 import 'package:vit_ap_student_app/models/widgets/home/my_weather_widget.dart';
 import 'package:vit_ap_student_app/models/widgets/home/quick_access.dart';
+import '../../models/widgets/home/my_home_appbar.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -17,97 +17,90 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: MyHomeAppBar(),
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
+      body: CustomScrollView(
+        slivers: [
+          MyHomeSliverAppBar(),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Lottie.asset(
-                    "assets/images/lottie/schedule.json",
-                    frameRate: FrameRate(60),
-                    width: 36,
-                  ),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Text(
-                    'Today\'s Schedule',
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w500,
-                        color: Theme.of(context).colorScheme.primary),
-                  ),
-                ],
-              ),
-
-              SizedBox(
-                height: 10,
-              ),
-
-              //Upcoming Classes
-              MyUpcomingClassWidget(),
-
-              //Weather Text
-              Row(
-                children: [
-                  Lottie.asset(
-                    "assets/weather_icons/umbrella.json",
-                    frameRate: FrameRate(60),
-                    width: 48,
-                  ),
-                  const Text(
-                    'Weather',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-
-              //Weather Widget
-              MyWeatherWidget(),
-
-              SizedBox(
-                height: 10,
-              ),
-
-              //Quick access
-              Stack(
-                children: [
-                  Positioned(
-                    top: -5,
-                    left: -16,
-                    child: Lottie.asset(
-                      "assets/images/lottie/cards.json",
-                      frameRate: FrameRate(60),
-                      width: 80,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 45.0),
-                    child: Text(
-                      'Quick access',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w500,
+                  // Today's Schedule
+                  Row(
+                    children: [
+                      Lottie.asset(
+                        "assets/images/lottie/schedule.json",
+                        frameRate: FrameRate(60),
+                        width: 36,
                       ),
-                    ),
+                      SizedBox(width: 4),
+                      Text(
+                        'Today\'s Schedule',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                    ],
                   ),
+                  SizedBox(height: 10),
+                  MyUpcomingClassWidget(),
+
+                  // Weather Text
+                  Row(
+                    children: [
+                      Lottie.asset(
+                        "assets/weather_icons/umbrella.json",
+                        frameRate: FrameRate(60),
+                        width: 48,
+                      ),
+                      const Text(
+                        'Weather',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  // Weather Widget
+                  MyWeatherWidget(),
+                  SizedBox(height: 10),
+
+                  // Quick access
+                  Stack(
+                    children: [
+                      Positioned(
+                        top: -5,
+                        left: -16,
+                        child: Lottie.asset(
+                          "assets/images/lottie/cards.json",
+                          frameRate: FrameRate(60),
+                          width: 80,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 45.0),
+                        child: Text(
+                          'Quick access',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  MyQuickAccess(),
                 ],
               ),
-              SizedBox(
-                height: 10,
-              ),
-              MyQuickAccess(),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
