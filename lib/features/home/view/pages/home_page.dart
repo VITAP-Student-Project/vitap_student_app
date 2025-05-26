325b0c06f9cc451bf7cfed92f85bc67ddbd7bf57
Udhay-Adithya <udhayxd@gmail.com>
2025-05-27 00:34:48 +0530

    feat: add home page headers

--- lib/features/home/view/pages/home_page.dart
+++ lib/features/home/view/pages/home_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vit_ap_student_app/core/providers/current_user.dart';
+import 'package:vit_ap_student_app/features/home/view/widgets/home_app_bar.dart';
+import 'package:vit_ap_student_app/features/home/view/widgets/home_greeting.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(currentUserNotifierProvider);
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const HomeAppBar(),
+          SliverToBoxAdapter(
+            child: HomeGreeting(
+              username: user?.profile.target?.studentName ?? "NaN",
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
