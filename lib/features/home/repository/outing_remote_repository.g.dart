f070bdf9c8d1754d1d311acdb27f484b252a65f3
Udhay-Adithya <udhayxd@gmail.com>
2025-11-15 23:47:45 +0530

    feat: implement outing remote repository with fetching and submitting reports functionality

---
+++ lib/features/home/repository/outing_remote_repository.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'outing_remote_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$outingRemoteRepositoryHash() =>
+    r'63e6c2b1e28c51d6db56233312cc73a886ec82cd';
+
+/// See also [outingRemoteRepository].
+@ProviderFor(outingRemoteRepository)
+final outingRemoteRepositoryProvider =
+    AutoDisposeProvider<OutingRemoteRepository>.internal(
+  outingRemoteRepository,
+  name: r'outingRemoteRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$outingRemoteRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef OutingRemoteRepositoryRef
+    = AutoDisposeProviderRef<OutingRemoteRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
