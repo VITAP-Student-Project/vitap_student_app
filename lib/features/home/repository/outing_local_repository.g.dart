23c8aa40db710040e4fbd984ac24c5d96345c86f
Udhay-Adithya <udhayxd@gmail.com>
2025-11-30 17:59:19 +0530

    feat: implement outing local repository for caching support

---
+++ lib/features/home/repository/outing_local_repository.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'outing_local_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$outingLocalRepositoryHash() =>
+    r'8edfb575618a55364cb33e972f88ab0c88c49b1d';
+
+/// See also [outingLocalRepository].
+@ProviderFor(outingLocalRepository)
+final outingLocalRepositoryProvider =
+    AutoDisposeProvider<OutingLocalRepository>.internal(
+  outingLocalRepository,
+  name: r'outingLocalRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$outingLocalRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef OutingLocalRepositoryRef
+    = AutoDisposeProviderRef<OutingLocalRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
