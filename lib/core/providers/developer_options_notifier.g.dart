64425a609c4b83365278c74b5c76d658e3d3ce5a
Udhay-Adithya <udhayxd@gmail.com>
2025-12-04 16:24:23 +0530

    feat: add developer options notifier with debug information loading

---
+++ lib/core/providers/developer_options_notifier.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'developer_options_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$developerOptionsNotifierHash() =>
+    r'62bfa63e7e74f3bb1d46533bd3cc8423c31f3ead';
+
+/// See also [DeveloperOptionsNotifier].
+@ProviderFor(DeveloperOptionsNotifier)
+final developerOptionsNotifierProvider = AutoDisposeAsyncNotifierProvider<
+    DeveloperOptionsNotifier, DeveloperOptionsState>.internal(
+  DeveloperOptionsNotifier.new,
+  name: r'developerOptionsNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$developerOptionsNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DeveloperOptionsNotifier
+    = AutoDisposeAsyncNotifier<DeveloperOptionsState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
