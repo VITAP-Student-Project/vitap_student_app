f565de4ccb1b3ab28dac1ad9f9f3964e8ce2ed53
Udhay-Adithya <udhayxd@gmail.com>
2026-01-22 01:53:38 +0530

    feat: add course page repository

---
+++ lib/features/course_page/repository/course_page_remote_repository.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'course_page_remote_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$coursePageRemoteRepositoryHash() =>
+    r'de9abbe9e0a2fcc4748881efc937b625b4eee25f';
+
+/// See also [coursePageRemoteRepository].
+@ProviderFor(coursePageRemoteRepository)
+final coursePageRemoteRepositoryProvider =
+    AutoDisposeProvider<CoursePageRemoteRepository>.internal(
+  coursePageRemoteRepository,
+  name: r'coursePageRemoteRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$coursePageRemoteRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CoursePageRemoteRepositoryRef
+    = AutoDisposeProviderRef<CoursePageRemoteRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
