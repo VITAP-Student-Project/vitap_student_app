db8b8a227f9f2cde04e8c45676e8ce21bec88382
Udhay-Adithya <udhayxd@gmail.com>
2026-03-02 17:45:23 +0530

    feat: implement digital assignment repository with methods for fetching, uploading, and downloading digital assignments

---
+++ lib/features/digital_assignment/repository/digital_assignment_remote_repository.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'digital_assignment_remote_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$digitalAssignmentRemoteRepositoryHash() =>
+    r'd0c753cebdbf26a2e9751552c43590fb8688ddf0';
+
+/// See also [digitalAssignmentRemoteRepository].
+@ProviderFor(digitalAssignmentRemoteRepository)
+final digitalAssignmentRemoteRepositoryProvider =
+    AutoDisposeProvider<DigitalAssignmentRemoteRepository>.internal(
+  digitalAssignmentRemoteRepository,
+  name: r'digitalAssignmentRemoteRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$digitalAssignmentRemoteRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef DigitalAssignmentRemoteRepositoryRef
+    = AutoDisposeProviderRef<DigitalAssignmentRemoteRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
