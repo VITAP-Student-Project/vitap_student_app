72263d8f114e16be678d0283787506d0581c24a1
Udhay-Adithya <udhayxd@gmail.com>
2026-03-02 23:18:10 +0530

    feat: implement github repository to fetch changelog and contributors

---
+++ lib/features/account/repository/github_repository.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'github_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$githubRepositoryHash() => r'920c206b925a049da6091cf407e09dc718d88bb2';
+
+/// See also [githubRepository].
+@ProviderFor(githubRepository)
+final githubRepositoryProvider = AutoDisposeProvider<GithubRepository>.internal(
+  githubRepository,
+  name: r'githubRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$githubRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef GithubRepositoryRef = AutoDisposeProviderRef<GithubRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
