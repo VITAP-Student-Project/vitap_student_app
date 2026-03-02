32f572f0f69a270ca512ea7d3d637391601e1739
Udhay-Adithya <udhayxd@gmail.com>
2026-03-02 23:44:41 +0530

    feat: add github view model

---
+++ lib/features/account/viewmodel/github_viewmodel.g.dart
@@ -0,0 +1,45 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'github_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$changelogViewModelHash() =>
+    r'fe9add1a942f829565f31404589e83d1b2fecf3f';
+
+/// See also [ChangelogViewModel].
+@ProviderFor(ChangelogViewModel)
+final changelogViewModelProvider = AutoDisposeNotifierProvider<
+    ChangelogViewModel, AsyncValue<String>?>.internal(
+  ChangelogViewModel.new,
+  name: r'changelogViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$changelogViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ChangelogViewModel = AutoDisposeNotifier<AsyncValue<String>?>;
+String _$contributorsViewModelHash() =>
+    r'62a69330fa3f7fedcad77ffc606b4dd9179157a0';
+
+/// See also [ContributorsViewModel].
+@ProviderFor(ContributorsViewModel)
+final contributorsViewModelProvider = AutoDisposeNotifierProvider<
+    ContributorsViewModel, AsyncValue<List<Contributor>>?>.internal(
+  ContributorsViewModel.new,
+  name: r'contributorsViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$contributorsViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ContributorsViewModel
+    = AutoDisposeNotifier<AsyncValue<List<Contributor>>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
