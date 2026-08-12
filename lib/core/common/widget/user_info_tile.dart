7e3fcda3c07fa1ae9f831b75c9e53a4443ae17a8
Ponsriram <ponsriramor@gmail.com>
2026-08-13 01:11:15 +0530

    feat(user_info_tile): add long press to copy description to clipboard

--- lib/core/common/widget/user_info_tile.dart
+++ lib/core/common/widget/user_info_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../../utils/show_snackbar.dart';
 
 class UserInfoTile extends StatelessWidget {
   final String title;
@@ -13,24 +15,39 @@ class UserInfoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-            color: Theme.of(context).colorScheme.primary,
+    final displayDescription = description == '' ? 'N/A' : description;
+    return GestureDetector(
+      onLongPress: () async {
+        if (displayDescription != 'N/A') {
+          await Clipboard.setData(ClipboardData(text: displayDescription));
+          if (context.mounted) {
+            showSnackBar(
+              context,
+              '$title copied to clipboard',
+              SnackBarType.success,
+            );
+          }
+        }
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: Theme.of(context).colorScheme.primary,
+            ),
           ),
-        ),
-        const SizedBox(height: 0),
-        Text(
-          description == '' ? 'N/A' : description,
-          style: Theme.of(context).textTheme.bodyLarge,
-        ),
-        SizedBox(height: gap)
-      ],
+          const SizedBox(height: 0),
+          Text(
+            displayDescription,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          SizedBox(height: gap),
+        ],
+      ),
     );
   }
 }
